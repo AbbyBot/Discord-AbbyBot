f7b35ab122ee11c89051039b948e0ee5292de40f
astronautmarkus <astronautmarkus@gmail.com>
2024-09-19 00:03:16 -0300

    Update Help.py

--- Python_Discord-AbbyBot/chat_commands/Help.py
+++ Python_Discord-AbbyBot/chat_commands/Help.py
@@ -4,6 +4,7 @@ from discord import app_commands
 import mysql.connector
 from dotenv import load_dotenv
 import os
+from embeds.embeds import account_inactive_embed
 
 # Load dotenv variables
 load_dotenv()
@@ -28,6 +29,36 @@ class Help(commands.Cog):
         guild_id = interaction.guild_id
         cursor.execute("SELECT guild_language FROM server_settings WHERE guild_id = %s", (guild_id,))
         result = cursor.fetchone()
+
+        # Get guild_id and user_id from the interaction
+        guild_id = interaction.guild_id
+        user_id = interaction.user.id
+
+        # Check if the user is active (is_active = 1) or inactive (is_active = 0)
+        cursor.execute("SELECT is_active FROM dashboard WHERE guild_id = %s AND user_id = %s", (guild_id, user_id))
+        result = cursor.fetchone()
+
+        if result is None:
+            await interaction.response.send_message("User not found in the database.", ephemeral=True)
+            cursor.close()
+            db.close()
+            return
+
+        # If the user is inactive (is_active = 0), send an embed in DM and exit
+        is_active = result[0]
+        if is_active == 0:
+            try:
+                # Send the embed message as a DM
+                await interaction.user.send(embed=account_inactive_embed())
+                print(f"User {interaction.user} is inactive and notified.")
+            except discord.Forbidden:
+                print(f"Could not send DM to {interaction.user}. They may have DMs disabled.")
+
+            await interaction.response.send_message("Request Rejected: Your account has been listed as **inactive** in the AbbyBot system, please check your DM.", ephemeral=True)
+
+            cursor.close()
+            db.close()
+            return
 
         if result is None:
             # if server is not registered, send error message
