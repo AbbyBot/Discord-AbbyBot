aabc750e5451d66e91dd5ae10ab2258ea931c9b3
astronautmarkus <astronautmarkus@gmail.com>
2024-09-18 23:24:05 -0300

    Update dog_img.py

--- Python_Discord-AbbyBot/api_commands/dog_img.py
+++ Python_Discord-AbbyBot/api_commands/dog_img.py
@@ -5,6 +5,7 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 import requests
+from embeds.embeds import account_inactive_embed
 
 # Load dotenv variables
 load_dotenv()
@@ -16,9 +17,6 @@ class DogImg(commands.Cog):
     @app_commands.command(name="dog_img", description="Show images of random dogs")
     async def dogimg(self, interaction: discord.Interaction):
 
-        # Defer the response to avoid the interaction timeout
-        await interaction.response.defer()
-
         # Connect to database with dotenv variables
         db = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
@@ -28,6 +26,39 @@ class DogImg(commands.Cog):
         )
         cursor = db.cursor()
 
+
+        # Get guild_id and user_id from the interaction
+        guild_id = interaction.guild_id
+        user_id = interaction.user.id
+
+        # Check if the user is active (is_active = 1) or inactive (is_active = 0)
+        cursor.execute("SELECT is_active FROM dashboard WHERE guild_id = %s AND user_id = %s", (guild_id, user_id))
+        result = cursor.fetchone()
+
+        if result is None:
+            await interaction.response.send_message("User not found in the database.", ephemeral=True)
+            cursor.close()
+            db.close()
+            return
+
+        # If the user is inactive (is_active = 0), send an embed in DM and exit
+        is_active = result[0]
+        if is_active == 0:
+            try:
+                # Send the embed message as a DM
+                await interaction.user.send(embed=account_inactive_embed())
+                print(f"User {interaction.user} is inactive and notified.")
+            except discord.Forbidden:
+                print(f"Could not send DM to {interaction.user}. They may have DMs disabled.")
+
+            await interaction.response.send_message("Request Rejected: Your account has been listed as **inactive** in the AbbyBot system, please check your DM.", ephemeral=True)
+
+            cursor.close()
+            db.close()
+            return
+        else: # user are not "banned"
+            await interaction.response.defer()
+        
         # Check if server is registered
         guild_id = interaction.guild_id
         cursor.execute("SELECT guild_language FROM server_settings WHERE guild_id = %s", (guild_id,))
