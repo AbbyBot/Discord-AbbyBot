dc11243dd74f1feadd2e97ee24eac2e18b624307
astronautmarkus <astronautmarkus@gmail.com>
2024-09-29 19:43:39 -0300

    Fixed batch script

--- 3. Start Windows environment.bat
+++ 3. Start Windows environment.bat
@@ -10,7 +10,7 @@ REM Change bot directory
 cd "%script_path%Python_AbbyBot_Second_phase"
 
 REM Start discord bot
-python "Abby-bot.py" run
+python "AbbyBot.py" run
 
 REM Once the bot is closed, deactivate the virtual environment
 deactivate
