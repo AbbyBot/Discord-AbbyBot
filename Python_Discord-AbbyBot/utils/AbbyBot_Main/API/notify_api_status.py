2c503b4d19b15fa62e1403eb0f6d7962b6d796a0
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-09 02:11:32 -0400

    fix: clean up update_bot_info function and include discriminator in API data

--- Python_Discord-AbbyBot/utils/AbbyBot_Main/API/notify_api_status.py
+++ Python_Discord-AbbyBot/utils/AbbyBot_Main/API/notify_api_status.py
@@ -26,30 +26,25 @@ def notify_api_status(status):
 
         print("\033[31m" + f"Error notifying API: {e}" + "\033[0m")
 
-
 def update_bot_info(bot):
     try:
         api_url = os.getenv("API_URL")
         if not api_url:
                     print("\033[31mAPI URL not found. Check your environment variables.\033[0m")
                     return
-
-        
         bot_id = bot.user.id
         bot_avatar = bot.user.avatar.url if bot.user.avatar else None
         bot_name = bot.user.name
-
-                # Data to be sent to the API
+        bot_discriminator = bot.user.discriminator
         data = {
                     "bot_id": bot_id,
                     "avatar_url": bot_avatar,
-                    "bot_name": bot_name
+                    "bot_name": bot_name,
+                    "discriminator": bot_discriminator,
                 }
-
         response = requests.post(api_url, json=data)
-
         if response.status_code == 200:
-                    print("\033[32m" + f"Bot info updated successfully" + "\033[0m")
+                    print("\033[33m" + f"Bot info updated successfully" + "\033[0m")
         else:
                     print("\033[33m" + f"Failed to update bot info. Status code: {response.status_code}, Response: {response.text}" + "\033[0m")
     except requests.exceptions.RequestException as e:
